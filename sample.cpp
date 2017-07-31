f4d995e4f9a22ce66f25388156d1c9b2166fdc23
MrEO_Desktop <mistereo@hotmail.com>
2017-07-31 21:40:17 +0800

    Finished basic function

--- sample.cpp
+++ sample.cpp
@@ -1,8 +1,13 @@
-#include "motion.h"
 #include "detector.h"
+
 int main(int argc, char ** argv)
 {
 	Detector::ins().insert("test");
 	Detector::ins().adjust_color("test");
+	while (true) {
+		Detector::ins().refresh();
+		Detector::ins().process("test");
+		Detector::ins().show();
+	}
 	return 0;
 }
